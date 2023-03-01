7504a35bf3db4242b216207ffc9a34c3515765e4
Federico <federuedaleal@hotmail.com>
2023-02-28 23:51:03 -0300

    Ejercicio  6 resuelto

---
+++ js/ejercicio6.js
@@ -0,0 +1,8 @@
+let num1 = parseInt(prompt('Ingrese primer numero'));
+let num2 = parseInt(prompt('Ingrese segundo numero'));
+if (num1 > num2) {
+    document.write(num1)
+}
+else {
+    document.write(num2)
+};
