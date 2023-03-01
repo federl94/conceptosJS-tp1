b98702121c9f6c34dbbe63793a1212831243695b
Federico <federuedaleal@hotmail.com>
2023-03-01 00:09:49 -0300

    Ejercicio 8 sin resolver

---
+++ js/ejercicio8.js
@@ -0,0 +1,7 @@
+let num = parseInt(prompt('Ingrese primer numero'))
+if (num/2 && num==parseInt) {
+    document.write('No es divisible por 2')
+}
+else {
+    document.write('Si es divisible por 2')
+}
